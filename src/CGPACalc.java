48c46308274144d6096670c867624ce3b246255d
Abere Fejiro <aberefejiro@gmail.com>
2021-01-27 09:05:58 +0100

    Initial commit

---
+++ src/CGPACalc.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+
+public class CGPACalc {
+
+
+    private JPanel panel1;
+    private JTextPane coursesTextPane;
+    private JTextPane courseCreditTextPane;
+    private JTextPane courseGradeTextPane;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+
+    public CGPACalc() {
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("CGPA Calculator")
+    }
+}
